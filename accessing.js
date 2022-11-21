73d15d8e7a47cae93c62ac67b15455251306f14a
kytsang <kytsang@uw.edu>
2022-11-20 17:49:52 -0800

    HW draft - Sunday

--- accessing.js
+++ accessing.js
@@ -1,5 +1,18 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
+const headline = document.getElementById('weather-head');
+
+headline.innerText ='February 10 Weather Forecast, Seattle';
+
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+const sunEls = document.querySelectorAll('.sun')
+
+sunEls.forEach((el) => el.style.color = 'orange')
+
+
+
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+const cloud = document.querySelector('.sun');
+cloud.className='cloudy';
